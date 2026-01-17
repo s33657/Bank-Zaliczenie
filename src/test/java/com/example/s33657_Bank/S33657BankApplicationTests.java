1f782a2c7a00671575e665f03419998b60d800f3
Marek Kowalski <marek.kowalski@test1.pl>
2026-01-17 12:08:36 +0100

    git commit -m "Dodanie klasy S3365BankAppicationTests.java "

---
+++ src/test/java/com/example/s33657_Bank/S33657BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s33657_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S33657BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
