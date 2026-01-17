6af57d307b347beddc6f3c88d0914f4da224d6aa
Marek Kowalski <marek.kowalski@test1.pl>
2026-01-17 12:05:14 +0100

    git commit -m "Dodanie klasy startowej aplikacji"

---
+++ src/main/java/com/example/s33657_Bank/S33657BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s33657_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S33657BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S33657BankApplication.class, args);
+	}
+
+}
